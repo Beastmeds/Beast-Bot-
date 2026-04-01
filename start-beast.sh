2c4ef233601683cf3f2be5e9294986e4f1fe502c
Beastmeds <beastmeds@gmail.com>
2026-04-01 16:08:54 +0200

    61

--- start-beast.sh
+++ start-beast.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Wrapper to provide a single '1' input to BeastBot.js so PM2 doesn't require interactive input.
 # PM2 will execute this script; it pipes '1' into the Node process and replaces the shell with node (exec).
-exec printf "1\n" | node "$(dirname "$0")/start.js"
+exec printf "a\n" | node "$(dirname "$0")/start.js"
